03bd325eb90a9b0d9047df7130156a7514b09cf9
fector101 <fabianjoseph063@gmail.com>
2025-01-19 18:04:33 +0100

    classes

--- grimoire/src/components/js/ImgwithPL.js
+++ grimoire/src/components/js/ImgwithPL.js
@@ -2,7 +2,7 @@ import { useState,useEffect } from "react";
 import placeholder_img_default from "./../imgs/card-img-pl1.png"
 
 
-export default function ImgwithPL({src, placeholder_src, alt}){
+export default function ImgwithPL({src, placeholder_src, alt,className}){
     const [is_loaded, setIsLoaded] = useState(0)
     placeholder_src= placeholder_src || placeholder_img_default
     useEffect(() => {
@@ -13,6 +13,7 @@ export default function ImgwithPL({src, placeholder_src, alt}){
 
     return (
         <img
+            className={className || ''}
             src={is_loaded? src : placeholder_src}
             alt={alt}
             loading="lazy"
