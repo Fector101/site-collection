77943ca3c154f693afad840ada27cfb77818a417
fector101 <fabianjoseph063@gmail.com>
2025-01-20 18:31:18 +0100

    HTML

--- grimoire/src/components/ui/footer/Footer.js
+++ grimoire/src/components/ui/footer/Footer.js
@@ -21,13 +21,13 @@ function Row({header,links,className}){
 }
 export default function Footer(){
     return(
-        <div className="footer flex">
+        <footer className="footer flex">
             {/* <img className="logo" src={logo_src} alt="logo" /> */}
             <Row className="basic" header="Grimorie" links={[{name_:"Lists",url:'lists'},{name_:'Help',url:'help'},{name_:"Chat",url:'chat'}]}/>
             <Row className="site-sections" header='Categories' links={[{url:'trending'},{url:'top'},{url:'ongoning'},{url:'upcoming'}]} />
             <Row className="company-sections" header="Company" links={[{url:'about'},{url:'contant-us'},{url:'terms', name_:'Terms of Service'},{url:'hire'}]} />
             <Row className="contant-handlers" header='' links={[{url:'twitter'},{url:'email'}]} />
-        </div>
+        </footer>
     )
 }
 // how to add th in linux mint time
