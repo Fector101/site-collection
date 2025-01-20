77b02583f798c408402b8a76de8709c9cd2251dc
fector101 <fabianjoseph063@gmail.com>
2025-01-20 17:15:32 +0100

    login btn

--- grimoire/src/components/ui/call-to-action/CalltoAction.js
+++ grimoire/src/components/ui/call-to-action/CalltoAction.js
@@ -8,25 +8,20 @@ export default function CalltoAction(){
             <h3>Login</h3>
             <p className="msg">For More Refined & Personalized Recommendations </p>
             <form className="flex">
-                {/* <div className="row flex"> */}
-                    <label htmlFor='email'> Email </label>
-                    <input id="email" placeholder="Enter your email"/>
-                {/* </div>
-                <div className="row flex"> */}
+                <label htmlFor='email'> Email </label>
+                <input id="email" placeholder="Enter your email"/>
+                
+                <label htmlFor='password'> Password </label>
+                <div className="password-input-box flex">
+                    <input type="password" id="password" placeholder="Enter your password"/>
+                    <button className="flex"> <Eye /> </button>
+                </div>
 
-                    <label htmlFor='password'> Password </label>
-                    <div className="password-input-box flex">
-                        <input type="password" id="password" placeholder="Enter your password"/>
-                        <button className="flex"> <Eye /> </button>
-                    </div>
-                {/* </div> */}
+                <button className="outline-white sign-in">Login</button>
             </form>
-            {/* <button className="outline-white sign-up">Sign Up</button> */}
             <div className="flex sign-up-box">
                 <p>Need an account?</p>
-                {/* <p>Login</p> */}
                 <Link className='sign-up-link' to='/signup'>Sign Up</Link>
-                {/* <button className="outline-white sign-in">Login</button> */}
             </div>
         </div>
     </div>
