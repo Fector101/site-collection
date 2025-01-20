fdbc484016e6ae174252d2270dd6d0de083a4e62
fector101 <fabianjoseph063@gmail.com>
2025-01-20 18:31:00 +0100

    add timeout design to not found page

--- grimoire/src/pages/NotFoundpage.js
+++ grimoire/src/pages/NotFoundpage.js
@@ -1,18 +1,38 @@
-import { useEffect } from "react";
+import { useEffect, useRef, useState } from "react";
 import { useParams, useNavigate } from "react-router-dom";
-
+import './../components/css/page-not-found.css'
 export default function NotFoundpage(){
     const {'*':url_extension} = useParams()
     const navigate = useNavigate()
+    const max_timeout=5
+    let [i, Seti] = useState(max_timeout)
+
     useEffect(function(){
+        Seti(max_timeout)
+        const interval = setInterval(()=>{
+            Seti(old_count=>{
+                console.log(old_count)
+                if (old_count === 1)clearInterval(interval)
+                return old_count-1
+            })
+            } ,1*1000)
         setTimeout(()=>{
             // navigate(-1) // To Go to last page.
-            navigate('/',{state: 'Page not found'})
+            // navigate('/',{state: 'Page not found'})
         },1000)
+
+        return () => clearInterval(interval)
     // eslint-disable-next-line
-    },[])
+    },[url_extension])
+
+
     return(
-        <p> {url_extension} does not exist, It a lie </p>
+        <div className="not-found-page-ui">
+            <p>Going back main page in</p>
+            <p style={{fontSize: '2.1rem'}}>{i} {i>1?'Secs':'Sec'}</p>
+            <p> {url_extension} does not exist</p>
+
+        </div>
         // To Redirect
         // <Navigate to="/grimoire"/>
     )
