9f8a5227f5ac0a6b190c98071f50c83991d2e1d8
fector101 <fabianjoseph063@gmail.com>
2025-01-21 13:47:02 +0100

    classes

--- grimoire/src/components/ui/buttons/buttons.js
+++ grimoire/src/components/ui/buttons/buttons.js
@@ -1,8 +1,8 @@
 import { Bookmark } from "lucide-react";
 
-function BookmarkActionButton(){
+function BookmarkActionButton({className}){
     return (
-        <button className='beating-bookmark-btn'>
+        <button className={'beating-bookmark-btn' + (className? ' '+className:'')}>
             <Bookmark className='svg-white-fill first-svg'/>
             <Bookmark className='svg-white-fill'/>
         </button>
