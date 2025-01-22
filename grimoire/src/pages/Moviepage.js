f176e97665e544127e8db0d0ca3b8b385e97b11c
fector101 <fabianjoseph063@gmail.com>
2025-01-22 22:18:56 +0100

    better name

--- grimoire/src/pages/Moviepage.js
+++ grimoire/src/pages/Moviepage.js
@@ -30,13 +30,13 @@ export default function Moviepage(){
         const clicked_btn = element.target.closest('button')
         if (!clicked_btn) return
 
-        const card = element.target.closest('.rate-btns-case')
+        const btn_box = element.target.closest('.rate-btns-case')
         const user_vote = [...clicked_btn.classList].filter(class_name=> ['up','down'].includes(class_name))[0]
 
         if (user_vote === 'up'){
-            card.querySelector('.vote-btn.down').classList.remove('clicked')
+            btn_box.querySelector('.vote-btn.down').classList.remove('clicked')
         }else if (user_vote === 'down'){
-            card.querySelector('.vote-btn.up').classList.remove('clicked')
+            btn_box.querySelector('.vote-btn.up').classList.remove('clicked')
         }
         clicked_btn.classList.toggle('clicked')
     }
