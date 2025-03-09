b524fbf447424b777d4272448f4b0e919a4ff65c
fector101 <fabianjoseph063@gmail.com>
2025-03-09 01:19:10 +0100

    class passing and formatting

--- grimoire/src/components/ui/login-signup/SignupComponent.js
+++ grimoire/src/components/ui/login-signup/SignupComponent.js
@@ -1,31 +1,33 @@
 import { Link } from "react-router-dom"
 import './../../css/form.css'
-import { InputCom,PasswordInput } from "../form-comps/Inputs"
+import { InputCom, PasswordInput } from "../form-comps/Inputs"
 
 
 
-export default function LoginComponent () {
+export default function SignupComponent({ className }) {
+    let additional_class = className ? ' ' + className : ''
+
     return (
-        <div id="signup-case" className="form-container">
+        <div id="signup-case" className={'form-container'+additional_class}>
             <h3>Signup</h3>
             <p className="form-top-msg">For More Refined & Personalized Recommendations </p>
-            
+
             <form className="flex">
 
-                <InputCom title="Email" type='email' placeholder="Enter your email"/>
-                <InputCom title="Username" type='text' placeholder="Mr. Wick"/>
+                <InputCom title="Email" type='email' placeholder="Enter your email" />
+                <InputCom title="Username" type='text' placeholder="Mr. Wick" />
                 <PasswordInput title="Password" placeholder="Enter your password" />
-                
+
                 <div className="form-row constent">
-                    <input type="checkbox" required/>
+                    <input type="checkbox" required />
                     <p> I accept the <Link to='policy'>Privacy Policy</Link> </p>
                 </div>
                 <button className="outline-white form-submit-btn">Signup</button>
             </form>
 
             <div className="form-btm-msg-section">
-                <p className="msg">Need an account?</p>
-                <Link className='sign-up-link' to='/signup'>Sign Up</Link>
+                <p className="msg">Already have an account?</p>
+                <Link className='sign-up-link' to='/signup'>Login</Link>
             </div>
 
         </div>
--- grimoire/src/pages/Signuppage.js
+++ grimoire/src/pages/Signuppage.js
@@ -1,10 +1,13 @@
 import SignUpCompoment from "../components/ui/login-signup/SignupComponent"
 import './../components/css/signuppage.css'
+import niceImg from "./../components/imgs/rotten_tomato.png"
+
+console.log(niceImg,' nice-img')
 export default function SignupPage(){
     return(
         <div className="signup-page flex-page">
-            <SignUpCompoment/>
-
+            <SignUpCompoment className="signup-form"/>
+            <div className="responsive-backdrop" style={{backgroundImage: `url(${niceImg})`}}></div>
         </div>
     )
 }
