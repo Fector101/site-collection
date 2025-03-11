054ba30b359745bdbd02b89fedd3bd979d87fab1
fector101 <fabianjoseph063@gmail.com>
2025-03-11 01:08:32 +0100

    All pages now have scroll to top

--- grimoire/src/pages/Homepage.js
+++ grimoire/src/pages/Homepage.js
@@ -1,26 +1,29 @@
-import {TrendingUp, Tv, ChartNoAxesColumn} from "lucide-react"
+import { TrendingUp, Tv, ChartNoAxesColumn } from "lucide-react"
 import Carousel from "../components/ui/carousel/Carousel"
 import SectionPreview from "./../components/js/SectionPreview"
 import Recommendations from "./../components/js/Recommendations"
 import LoginComponent from "../components/ui/login-signup/LoginForm"
+import GoToTop from "../components/js/GoToTop";
 
-export default function Homepage({top_movies_data__}){
+export default function Homepage({ top_movies_data__ }) {
 
     return (
         <div className="home-page page">
             <main>
-                {top_movies_data__&&<Carousel data={top_movies_data__.results?.slice(0,7)}/>}
+                {top_movies_data__ && <Carousel data={top_movies_data__.results?.slice(0, 7)} />}
             </main>
-            {top_movies_data__&&
-            <>
-            <SectionPreview title={'Trending'} icon={<TrendingUp />} data_info={{types:['Movies','TV Shows'],active:'Movies'}} data={top_movies_data__.results} url='trending'/>
-            <SectionPreview title={'Top'} icon={<ChartNoAxesColumn />} data_info={{types:['Movies','TV Shows'],active:'Movies'}} data={top_movies_data__.results} url='top'/>
-            <SectionPreview title={'Ongoing'} icon={<Tv />} data_info={{types:['TV Shows']}} data={top_movies_data__.results} url='ongoing'/>
-            <Recommendations data={top_movies_data__.results.slice(0,6)}/>
-            <div className="SectionPreview">
-                <LoginComponent />
-            </div>
-            </>
+            {top_movies_data__ &&
+                <>
+                    <SectionPreview title={'Trending'} icon={<TrendingUp />} data_info={{ types: ['Movies', 'TV Shows'], active: 'Movies' }} data={top_movies_data__.results} url='trending' />
+                    <SectionPreview title={'Top'} icon={<ChartNoAxesColumn />} data_info={{ types: ['Movies', 'TV Shows'], active: 'Movies' }} data={top_movies_data__.results} url='top' />
+                    <SectionPreview title={'Ongoing'} icon={<Tv />} data_info={{ types: ['TV Shows'] }} data={top_movies_data__.results} url='ongoing' />
+                    <Recommendations data={top_movies_data__.results.slice(0, 6)} />
+                    <div className="SectionPreview">
+                        <LoginComponent />
+                    </div>
+                    <GoToTop />
+
+                </>
             }
         </div>
     )
