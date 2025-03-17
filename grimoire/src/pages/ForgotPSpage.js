3ffd8743eed163d3f29c93c609ca4765e7f9efea
fector101 <fabianjoseph063@gmail.com>
2025-03-17 01:22:59 +0100

    removed dumb fea

--- grimoire/src/pages/ForgotPSpage.js
+++ grimoire/src/pages/ForgotPSpage.js
@@ -1,6 +1,9 @@
-import { Dot } from 'lucide-react'
+import { CheckCircle, Dot, Mail } from 'lucide-react'
 import '../components/css/forgotpspage.css'
-import { useState } from 'react'
+import { useEffect, useState } from 'react'
+import { InputCom } from '../components/ui/form-comps/Inputs'
+import './../components/css/form.css'
+
 function SendRightEmail() {
     const [email1, setEmail1] = useState('')
     const [email2, setEmail2] = useState('')
@@ -27,20 +30,61 @@ function SendRightEmail() {
         </div>
     )
 }
+
+
+function formatTime(seconds) {
+    const minutes = Math.floor(seconds / 60);
+    const secs = seconds % 60;
+    return `${String(minutes).padStart(2, '0')}:${String(secs).padStart(2, '0')}`;
+}
+
+
 export default function ForgotPSPage({ email, username, phone_no }) {
     const identifier = username || email || phone_no
+    const [count_down, setCountDown] = useState(10)
     username = 1
+    email = 'fabi*****063@gmail.com'
     // if `username` get right and wrong emails and display choices
     // `email` sent back will be genrated by from the username
     //  that is the first and last 4 letters for the email and i won't send link if email is wrong 
     // so i won't be flagged as spammer
+    useEffect(function () {
+        function countDown() {
+            setCountDown(old_value => {
+                if (old_value <= 1) {
+                    clearInterval(timer)
+                    return 0
+                }
+                return old_value - 1
+            })
+        }
+        const timer = setInterval(countDown, 1 * 1000)
+        return () => clearInterval(timer)
+    }, [])
     return (
         <div className="forgot-password-page flex-page margin-auto flex">
             <h1>Forgot Password {identifier} </h1>
             <div className="flex prompt-box">
+                {/* <div className="flex prompt-box"> */}
                 {
                     username &&
-                    <SendRightEmail />
+                    <>
+                        <CheckCircle className='sent-icon' />
+                        <h3>Successfully</h3>
+                        <h3>Sent Link to Reset To:</h3>
+                        <p className='sent-email'>
+                            <Mail/>
+                            {email}</p>
+                        <p className='long-msg'>
+                            Check your email and click on the link to reset your password. The link will expire in 1 hour.
+                        </p>
+                        <p className='didnt-recieve-msg'>Didn't recieve Link? </p>
+                        <button className={'outline-white resend' + (count_down ? ' disabled-btn' : '')}>
+                            {count_down ? formatTime(count_down) : 'Resend'}
+                        </button>
+                    </>
+                    // <InputCom title="Email" type='text' placeholder="Enter your email" />
+                    // <SendRightEmail />
 
                 }
 
