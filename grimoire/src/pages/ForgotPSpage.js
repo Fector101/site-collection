082c20b4a54eedbb498ed5bd9d2e968ed921dbdb
fector101 <fabianjoseph063@gmail.com>
2025-03-17 02:54:03 +0100

    refactor: enhance Forgot Password page layout and functionality

--- grimoire/src/pages/ForgotPSpage.js
+++ grimoire/src/pages/ForgotPSpage.js
@@ -2,7 +2,6 @@ import { CheckCircle, Dot, Mail } from 'lucide-react'
 import '../components/css/forgotpspage.css'
 import { useEffect, useState } from 'react'
 import { InputCom } from '../components/ui/form-comps/Inputs'
-import './../components/css/form.css'
 
 function SendRightEmail() {
     const [email1, setEmail1] = useState('')
@@ -31,6 +30,29 @@ function SendRightEmail() {
     )
 }
 
+function SentResetPSEmail({ count_down, email }) {
+    // Add Loading Snipper When Pinging Server Or Show Boxes or widgets Loading
+
+    return (
+        // <div className='sent-password-reset-email'>
+        <>
+            <CheckCircle className='sent-icon' />
+            <h3>Successfully</h3>
+            <h3>Sent Link to Reset To:</h3>
+            <p className='sent-email'>
+                <Mail />
+                {email}</p>
+            <p className='long-msg'>
+                Check your email and click on the link to reset your password. The link will expire in 1 hour.
+            </p>
+            <p className='didnt-recieve-msg'>Didn't recieve Link? </p>
+            <button className={'outline-white submit' + (count_down ? ' disabled-btn' : '')}>
+                {count_down ? formatTime(count_down) : 'Resend'}
+            </button>
+        </>
+        // </div>
+    )
+}
 
 function formatTime(seconds) {
     const minutes = Math.floor(seconds / 60);
@@ -38,10 +60,36 @@ function formatTime(seconds) {
     return `${String(minutes).padStart(2, '0')}:${String(secs).padStart(2, '0')}`;
 }
 
+function ChoicesBox({ email,user_choice,setChoice,setSlide }) {
+    // user_choice:'sign-in' | 'reset-ps'
+    function handleSettedChoice(event) {
+        const clicked_btn = event.target
+        if (clicked_btn) {
+            setChoice(user_choice === 'sign-in'?'reset-ps':'sign-in')
+            event.target.closest('.choices-btns-box').querySelector('.selected')?.classList.remove('selected')
+            // document.querySelector('.reset-ps-choice-form .submit').classList.remove('disabled-btn')
+        }
+        event.target.classList.add('selected')
+    }
+    return (
+        <>
+            <h3>Recovery your Account</h3>
+            <p className='long-msg'>Choose an option to access your account</p>
+            <div className='choices-btns-box'>
+                <button onClick={handleSettedChoice} className={'choice outline-white'+ (user_choice === 'reset-ps'?' selected':'')}>Reset password</button>
+                <button onClick={handleSettedChoice} className={'choice outline-white'+ (user_choice === 'sign-in'?' selected':'')}>Sign-in without password</button>
+            </div>
+            <p className='sent-email'>Email: {email}</p>
+            <button onClick={()=>setSlide(1)} className="mt-8px outline-white submit">{user_choice === 'sign-in'?'Send Sign-in Link': 'Send Reset Link'}</button>
 
+        </>
+    )
+}
 export default function ForgotPSPage({ email, username, phone_no }) {
     const identifier = username || email || phone_no
     const [count_down, setCountDown] = useState(10)
+    const [user_choice, setUserChoice] = useState('reset-ps') // 'sign-in' | 'reset-ps'
+    const [pos, setPos] = useState(0) // 'sign-in' | 'reset-ps'
     username = 1
     email = 'fabi*****063@gmail.com'
     // if `username` get right and wrong emails and display choices
@@ -65,27 +113,10 @@ export default function ForgotPSPage({ email, username, phone_no }) {
         <div className="forgot-password-page flex-page margin-auto flex">
             <h1>Forgot Password {identifier} </h1>
             <div className="flex prompt-box">
-                {/* <div className="flex prompt-box"> */}
                 {
-                    username &&
-                    <>
-                        <CheckCircle className='sent-icon' />
-                        <h3>Successfully</h3>
-                        <h3>Sent Link to Reset To:</h3>
-                        <p className='sent-email'>
-                            <Mail/>
-                            {email}</p>
-                        <p className='long-msg'>
-                            Check your email and click on the link to reset your password. The link will expire in 1 hour.
-                        </p>
-                        <p className='didnt-recieve-msg'>Didn't recieve Link? </p>
-                        <button className={'outline-white resend' + (count_down ? ' disabled-btn' : '')}>
-                            {count_down ? formatTime(count_down) : 'Resend'}
-                        </button>
-                    </>
-                    // <InputCom title="Email" type='text' placeholder="Enter your email" />
-                    // <SendRightEmail />
-
+                    pos===0?
+                    <ChoicesBox email={email} user_choice={user_choice} setSlide={setPos} setChoice={setUserChoice}/>
+                    :<SentResetPSEmail count_down={count_down} email={email} /> 
                 }
 
                 {/* <div className="login-without-ps">
@@ -101,9 +132,8 @@ export default function ForgotPSPage({ email, username, phone_no }) {
 
             </div>
             <span className='flex progress-box'>
-                <button> <Dot /> </button>
-                <button> <Dot /> </button>
-                <button> <Dot /> </button>
+                <button> <Dot className={pos===0?'active':''}/> </button>
+                <button> <Dot className={pos===1?'active':''}/> </button>
             </span>
         </div >
     )
