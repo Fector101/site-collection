2521239c09610e8f704b3be466c3e4d17dc3bcac
fector101 <fabianjoseph063@gmail.com>
2025-06-08 15:49:48 +0100

    feat: enhance AddToList component with create watchlist functionality and styling improvements

--- frontend/src/ui/popups/AddToList.tsx
+++ frontend/src/ui/popups/AddToList.tsx
@@ -1,7 +1,7 @@
 // It will get data by itself from context file or straight from server
 import { List, Plus, Search, X } from 'lucide-react'
 import './addtolist.css'
-import { useEffect, useState,useContext } from 'react';
+import { useEffect, useState, useContext, useCallback } from 'react';
 import { UserContext } from "../../assets/js/UserContextInstance";
 import { IWatchList } from '../../assets/js/myTypes';
 import ImgwithPL from '../images/ImgwithPL';
@@ -20,27 +20,57 @@ function SearchInput1({ placeholder }: { placeholder: string }) {
         </div>
     );
 }
+function CreateListPopup({ setAddToListStateToClose }: { setAddToListStateToClose: (event: React.MouseEvent<HTMLButtonElement>) => void }) {
+    function handleSubmit(event: React.FormEvent<HTMLFormElement>) {
+        event.preventDefault();
+        // Handle form submission logic here
+        console.log('Form submitted');
+    }
+    return (
+        <div className="add-to-list-popup popup-modal flex create-list-popup">
+            <main>
+                <button className='close-btn' onClick={setAddToListStateToClose} title='Close'>
+                    <X />
+                </button>
+                <h3>Create new watchlist</h3>
+                <form action="post" onSubmit={handleSubmit} className='flex fd-column'>
+                    <label htmlFor="watch-title">Watchlist Name</label>
+                    <input type="text" id='watch-title' placeholder='Enter watchlist name' />
+                    <label htmlFor="watch-title">Status</label>
+                    <select name="status" id="status">
+                        <option value="Watching">Watching</option>
+                        <option value="Completed">Completed</option>
+                        <option value="Planning">Planning</option>
+                        <option value="Public Lists">Public Lists</option>
+                    </select>
+                    <div className="btm-action-btns">
+                        <button className="cursor-pointer flex algin-items-cen cancel-btn" onClick={setAddToListStateToClose} title='Close'>Cancel</button>
+                        <button className="cursor-pointer flex algin-items-cen fill-btn" type='submit' title='Create'>Create Watchlist</button>
+                    </div>
+                </form>
+            </main>
+        </div>
+    );
+}
 export default function AddToList({ itemId, item_name }: IAddToListProps) {
+    const context = useContext(UserContext);
     const [watchlists, setWatchLists] = useState<IWatchList[] | undefined>()
+    const [create_list_state, setCreateListState] = useState<boolean>(() => false)
     console.log('Use itemId to add to database:', itemId)
 
-        const context = useContext(UserContext);
-        function setAddToListStateToClose(event: React.MouseEvent<HTMLButtonElement>) {
-            event.stopPropagation()
-            context?.setAddToListState(old => {
-                return { ...old, state: false}
-            })
-            // context?.setAddToListState(old => {
-            //     return { ...old, state: !old.state, itemId: itemId, item_name: item_name }
-            // })
-            
-        }
-    function clickedOption(event: React.MouseEvent<HTMLLIElement>,id:number) {
-        // event.stopPropagation()
+    function setAddToListStateToClose(event: React.MouseEvent<HTMLButtonElement>) {
+        event.stopPropagation()
+        close()
+
+    }
+    const close = useCallback(() => {
+        context?.setAddToListState(old => {
+            return { ...old, state: false }
+        })
+    }, [context]);
+    function clickedOption(event: React.MouseEvent<HTMLLIElement>, id: string) {
         document.querySelector('.add-to-list-item.active')?.classList.remove('active');
-        const clicked_list = event.target as HTMLElement
-        clicked_list.closest('li.add-to-list-item')?.classList.add('active');
-            // context?.addItemToList(itemId, listId);
+        (event.target as HTMLElement).closest('li.add-to-list-item')?.classList.add('active');
 
         if (id) {
             console.log(`Clicked on list with ID: ${id}`);
@@ -48,6 +78,31 @@ export default function AddToList({ itemId, item_name }: IAddToListProps) {
             // context?.addItemToList(itemId, listId);
         }
     }
+
+    useEffect(() => {
+        // Attaching ESC key event listener to close the popup
+        const handleKeyDown = (event: KeyboardEvent) => {
+            if (event.key !== 'Escape') { return }
+            console.log(create_list_state)
+            if (create_list_state) {
+                setCreateListState(false);
+            }
+            else {
+                close();
+                console.log('Add to list popup closed');
+            }
+        };
+        document.addEventListener('keyup', handleKeyDown);
+        // Cleanup the event listener on component unmount
+        return () => {
+            document.removeEventListener('keyup', handleKeyDown);
+        };
+        // List useful i think context is causing re-rendering setting create_list_state to false
+        //  even with const [create_list_state, , setCreateListState] = useState<boolean>(() => false)
+    }, [create_list_state, close]);
+
+
+
     // const [current_filter, setCurrentFilter] = useState<string>("All Lists")
     useEffect(function () {
         setWatchLists([
@@ -65,42 +120,48 @@ export default function AddToList({ itemId, item_name }: IAddToListProps) {
         ])
     }, [])
     return (
-        <div className="add-to-list-popup popup-modal flex">
-            <main className='flex'>
-                <button className='close-btn' onClick={setAddToListStateToClose} title='Close'>
-                    <X />
-                </button>
-                <h3 className='truncate-1-line'>Add "{item_name||'Inception'}" to a watchlist</h3>
-                <SearchInput1 placeholder='Search your watchlists' />
-                <ol>
-                    {
-                        watchlists?.map((list,i) => {
-                            return (
-                                <li key={list._id} className="add-to-list-item flex cursor-pointer" onClick={(e)=>clickedOption(e,list._id)}>
-                                    <ImgwithPL src={`https://picsum.photos/200/300?random=${list._id}`} alt={list.title} />
-                                    <div className="desc">
-                                        <p className="title">{list.title}</p>
-                                        <div className="count-box flex algin-items-cen">
-                                            <List />
-                                            <p className="count">{list.length} items</p>
-                                        </div>
-                                    </div>
-                                    <WatchListMarker text={list.status[0]} />
-                                </li>
-                            )
-                        })
-                    }
-                </ol>
-                <button className='cursor-pointer create-btn'>
-                    <Plus/>
-                    <p>Create new watchlist</p>
-                </button>
-                <div className="btm-action-btns flex">
-                    <button className="cursor-pointer flex algin-items-cen cancel-btn" onClick={setAddToListStateToClose} title='Close'>Cancel</button>
-                    <button className="cursor-pointer flex algin-items-cen add-btn">Add to watchlist</button>
-                </div>
-            </main>
+        <>
+            <div className="add-to-list-popup popup-modal flex">
 
-        </div>
+                <main className='flex'>
+                    <button className='close-btn' onClick={setAddToListStateToClose} title='Close'>
+                        <X />
+                    </button>
+                    <h3 className='truncate-1-line'>Add "{item_name || 'Inception'}" to a watchlist</h3>
+                    <SearchInput1 placeholder='Search your watchlists' />
+                    <ol>
+                        {
+                            watchlists?.map(list => {
+                                return (
+                                    <li key={list._id} className="add-to-list-item flex cursor-pointer" onClick={(e) => clickedOption(e, list._id)}>
+                                        <ImgwithPL src={`https://picsum.photos/200/300?random=${list._id}`} alt={list.title} />
+                                        <div className="desc">
+                                            <p className="title">{list.title}</p>
+                                            <div className="count-box flex algin-items-cen">
+                                                <List />
+                                                <p className="count">{list.length} items</p>
+                                            </div>
+                                        </div>
+                                        <WatchListMarker text={list.status[0]} />
+                                    </li>
+                                )
+                            })
+                        }
+                    </ol>
+                    <button className='cursor-pointer create-btn' onClick={() => setCreateListState(true)} title='Create new watchlist'>
+                        <Plus />
+                        <p>Create new watchlist</p>
+                    </button>
+                    <div className="btm-action-btns flex">
+                        <button className="cursor-pointer flex algin-items-cen cancel-btn" onClick={setAddToListStateToClose} title='Close'>Cancel</button>
+                        <button className="cursor-pointer flex algin-items-cen fill-btn">Add to watchlist</button>
+                    </div>
+                </main>
+
+            </div>
+            {create_list_state && <CreateListPopup setAddToListStateToClose={() => setCreateListState(false)} />}
+
+        </>
+
     )
 }
